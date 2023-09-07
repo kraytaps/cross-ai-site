77dfd5adb891a9dcff1dc4fe603348a9e10e5c4f
Chris <chris.791.ix@gmail.com>
2023-09-07 03:24:26 -0700

    testing open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,27 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import openGraphImg from "@/assets/openGraphImg.jpg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Cross AI - Bridge the Gap Between Data and Actionable Insights",
+	title: "Cross AI - Bridge the Gap Between Your Data and Actionable Insights",
 	description:
 		"Cross AI bridges the gap between your data and actionable insights. We lead your business through the AI frontier and transform it by harnessing the power of AI.",
+	openGraph: {
+		type: "website",
+		url: "https://xcross.ai",
+		title: "Cross AI - Bridge the Gap Between Your Data and Actionable Insights",
+		description:
+			"Our tools aim to bridge the gap between your data and actionable insights. Our platform is designed to lead your business through the AI frontier and transform it by harnessing the power of AI.",
+		siteName: "Cross AI",
+		images: [
+			{
+				url: "@/assets/openGraphImg.jpg",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
